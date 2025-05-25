a4e2a4553500f25fbd37e707f4767c1ccd0f0684
ephraim <ephraims.818@gmail.com>
2025-05-26 05:27:47 +0600

    JS-REV--NUMBERS-DATES-TIMERS - 04. Numeric Separators

---
+++ 04. Numeric Separators/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee = 15_00;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
